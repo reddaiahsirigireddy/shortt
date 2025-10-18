eb7e62e39b1538e4d7125ed10e07247e56707b3b
sjanapati9-glitch <sjanapati9@gmail.com>
2025-10-18 09:40:22 +0530

    Update Twitter link styling and separator

--- app/components/home/Twitter.vue
+++ app/components/home/Twitter.vue
@@ -10,13 +10,10 @@ const { twitter } = useAppConfig()
     :href="twitter"
     target="_blank"
     title="X(Twitter)"
-    class="inline-flex items-center px-3 py-1 mx-auto my-4 space-x-1 text-sm font-medium rounded-lg bg-muted"
+    class="inline-flex items-center px-4 py-2 mx-auto my-4 space-x-2 text-sm font-medium rounded-lg bg-gradient-to-r from-blue-400 to-blue-600 text-white transition-all duration-300 hover:scale-105 hover:shadow-lg"
   >
     <XIcon class="w-4 h-4" />
-    <Separator
-      orientation="vertical"
-      class="h-4"
-    />
+    <Separator orientation="vertical" class="h-4 bg-white/30" />
     <span>{{ $t('home.twitter.follow') }}</span>
     <ArrowRight class="w-4 h-4" />
   </a>
