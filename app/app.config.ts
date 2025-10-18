091806b893ce81afdf2ba573fa12b276e971ed95
sjanapati9-glitch <sjanapati9@gmail.com>
2025-10-18 10:12:39 +0530

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,13 +1,12 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
+  title: 'urlsclickearn',
+  email: 'contact@urlsclickearn.xyz',
   github: 'https://github.com/ccbikai/sink',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
+  telegram: 'https://urlsclickearn.xyz/telegram',
+  mastodon: 'https://urlsclickearn.xyz/mastodon',
+  blog: 'https://urlsclickearn.xyz/blog',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  image: 'https://urlsclickearn.xyz/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
